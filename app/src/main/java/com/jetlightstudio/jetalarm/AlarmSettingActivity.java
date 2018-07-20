f3d4393c47926b7a1ceb17c4153e2a066c650046
Oussama BONNOR <oussamabonnor@gmail.com>
2018-07-20 20:31:02 +0100

    Alarm broadcast receiver implemented

--- app/src/main/java/com/jetlightstudio/jetalarm/AlarmSettingActivity.java
+++ app/src/main/java/com/jetlightstudio/jetalarm/AlarmSettingActivity.java
@@ -1,13 +1,46 @@
 package com.jetlightstudio.jetalarm;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class AlarmSettingActivity extends AppCompatActivity {
+
+    TimePicker timePicker;
+    AlarmManager alarmManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_setting);
+
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        timePicker = findViewById(R.id.timePicker);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void setAlarm(View v){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
+        calendar.set(Calendar.MINUTE, timePicker.getMinute());
+        String time= String.format("%02d:%02d",calendar.get(Calendar.HOUR_OF_DAY) ,calendar.get(Calendar.MINUTE));
+        Intent intent = new Intent(getApplicationContext(), AlarmBroadcastReceiver.class);
+        intent.putExtra("time",time);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),pendingIntent);
+    }
+
+    public void cancelAlarm(View v){
+        Intent i = new Intent(getApplicationContext(), AlarmCreationActivity.class);
+        startActivity(i);
     }
 }
